5b4dd8d3559e6ca0ee5be8f426921e93dfb16df0
Veronika555 <vskorepanova@mail.ru>
2022-11-27 23:19:55 +0300

    test commit

--- python1/authdeskf/views.py
+++ python1/authdeskf/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Всем привет')
+    return HttpResponse('Всем здравствуйте')
 # Create your views here.
