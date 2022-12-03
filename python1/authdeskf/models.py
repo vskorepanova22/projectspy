411da4b00a29b3ff5302ede9bf869740ab369b62
Veronika <vskorepanova22@gmail.com>
2022-12-03 20:35:58 +0300

    test_test commit

--- python1/authdeskf/models.py
+++ python1/authdeskf/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    body = models.TextField()
+    dttm_add = models.DataTimeField(auto_now=True)
+    author= models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
